e4291e5d0674b77f73d535b05f9009841ec3476e
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2022-09-03 02:09:21 +0600

    add product init

--- src/components/Admin/products/AddProduct.jsx
+++ src/components/Admin/products/AddProduct.jsx
@@ -1,35 +1,77 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminLayout from '../../layout/AdminLayout'
 import { v4 as uuidv4 } from 'uuid';
 import { TrashIcon } from '@heroicons/react/solid';
-
+import { Router } from 'next/router';
+import { TagsInput } from "react-tag-input-component";
+import axiosAPI from '../../utils/axios-api';
+// import style from './styles'
 
 const Detail = () => {
 
     const [formValues, setFormValues] = React.useState([{ id: uuidv4(), detail: "" }])
-    const [moreInfos, setMoreInfo] = React.useState([{ id: uuidv4(), info: "" }])
-    // const [reviews, setReviews] = React.useState([{ id: uuidv4(), review: "" }])
+    const [moreInfos, setMoreInfo] = React.useState([{ id: uuidv4(), title: '', description: "" }])
+    const [featured, setFeatured] = React.useState(false)
+    const [error, setError] = React.useState('')
+    const [loading, setIsLoading] = React.useState(false)
+    const [tags, setTags] = useState(["Tech"]);
+    const [tagData, setTagData] = useState([{ id: uuidv4(), name: '' }])
+    const [selectedFiles, setSelectedFiles] = useState('');
 
-    // function handleChange(i, e) {
-    //   let newFormValues = [...formValues];
-    //   newFormValues[i][e.target.name] = e.target.value;
-    //   setFormValues(newFormValues);
-    // }
+    const [userInfo, setuserInfo] = useState({
+        file: [],
+        filepreview: null,
+    });
 
-    // function addFormFields() {
-    //   setFormValues([...formValues, { name: "", email: "" }])
-    // }
+    const handleSelectImage = async (e) => {
 
-    // function removeFormFields(i) {
-    //   let newFormValues = [...formValues];
-    //   newFormValues.splice(i, 1);
-    //   setFormValues(newFormValues)
-    // }
+        setSelectedFiles(e.target.files);
+        setuserInfo({
+            ...userInfo,
+            file: e.target.files[0],
+            filepreview: URL.createObjectURL(e.target.files[0]),
+        })
+    }
 
-    // function handleSubmit(event) {
-    //   event.preventDefault();
-    //   // alert(JSON.stringify(formValues));
-    // }
+    // submit form data
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        setTagData([
+            ...tagData,
+            {
+                id: uuidv4(),
+                name: tags
+            }
+        ])
+        try {
+            const data = new FormData(event.currentTarget);
+
+            const reqData = {
+                name: data.get('name'),
+                code: data.get('code'),
+                price: data.get('price'),
+                quantity: data.get('quantity'),
+                category: data.get('category'),
+                tags: JSON.stringify(tagData),
+                isFeatured: featured,
+                details: JSON.stringify(formValues),
+                informations: JSON.stringify(moreInfos),
+
+                images: Array.from(selectedFiles).forEach(file => {
+                    data.get(file);
+                })
+            };
+
+            console.log(reqData)
+
+            await axiosAPI.post('/products', reqData);
+            Router.push('/admin/products')
+        } catch (error) {
+            setIsLoading(false);
+            console.log(error)
+            setError(error.response?.data?.message)
+        }
+    }
 
     // Details 
     const handleChange = (id, event) => {
@@ -57,32 +99,6 @@ const Detail = () => {
         setFormValues(values);
     }
 
-    // Reviews 
-    // const handleReview = (id, event) => {
-    //     const newInputFields = reviews.map(i => {
-    //         if (id === i.id) {
-    //             i[event.target.name] = event.target.value
-    //         }
-    //         return i;
-    //     })
-
-    //     setReviews(newInputFields);
-    // };
-
-    // const addReview = () => {
-    //     setReviews([...reviews,
-    //     {
-    //         id: uuidv4(),
-    //         review: ''
-    //     }])
-    // };
-
-    // const removeReview = id => {
-    //     const values = [...reviews];
-    //     values.splice(values.findIndex(value => value.id === id), 1);
-    //     setReviews(values);
-    // }
-
     // More Information 
     const handleMoreinfo = (id, event) => {
         const newInputFields = moreInfos.map(i => {
@@ -109,33 +125,33 @@ const Detail = () => {
         setMoreInfo(values);
     }
 
-    const [tag, setTag] = useState(['ahnaf', 'istiak', 'tanvir'])
-    function handleTag() {
-
-    }
-    const tags = [
-        "Gold",
-        "Platinum",
-        "Silver",
-        "Copper",
-        "Steel",
-        "Aluminium",
-    ];
-
-
-
+    //Tags
+    // useEffect(() => {
+    //     setTagData([
+    //         ...tagData,
+    //         {
+    //             id: uuidv4(),
+    //             name: tags
+    //         }
+    //     ])
+    // })
     return (
 
         <div className='grid p-5 bg-white rounded-lg grid-cols-1 gap-3 justify-around m-3'>
-            <form>
+            {error && (
+                <div class="p-3 my-2 text-sm text-red-700 bg-yellow-100 rounded-lg" role="alert">
+                    <span class="font-medium">Warning!</span> {error}
+                </div>
+            )}
+            <form onSubmit={handleSubmit}>
                 <h1 className='text-center py-3 mb-5 rounded-lg bg-gray-200 text-2xl'>Add Product</h1>
                 <div className="grid gap-6 mb-6 md:grid-cols-2">
 
-                    {/* Details  */}
+                    {/* Product Details  */}
                     <div>
                         <div>
                             <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Product name</label>
-                            <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="" required />
+                            <input type="text" name='name' id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="" required />
                         </div>
                         {/* <div>
                             <label htmlFor="brand" className="block mb-2 text-sm font-medium text-gray-900 ">Brand</label>
@@ -143,32 +159,29 @@ const Detail = () => {
                         </div> */}
                         <div>
                             <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900">Category</label>
-                            <input type="text" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="" required />
+                            <input type="text" name='category' id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="" required />
                         </div>
                         <div>
                             <label htmlFor="code" className="block mb-2 text-sm font-medium text-gray-900 ">Product code</label>
-                            <input type="text" id="code" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+                            <input type="text" name='code' id="code" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="enter code" required />
                         </div>
                         <div>
-                            <label htmlFor="qty" className="block mb-2 text-sm font-medium text-gray-900">Quantity</label>
-                            <input type="number" id="qty" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="" required />
+                            <label htmlFor="quantity" className="block mb-2 text-sm font-medium text-gray-900">Quantity</label>
+                            <input type="number" name='quantity' id="quantity" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="" required />
                         </div>
                         <div>
                             <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900">Price</label>
-                            <input type="number" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="" required />
+                            <input type="number" name='price' id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5" placeholder="" required />
                         </div>
 
-                        
                         <div>
-                            <label className='block mb-2 text-sm font-medium text-gray-900' name="tags">Tags</label>
-                            <input
-                                multiple
-                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5"
-                                mode="tags"
-                                name='tags'
-                                style={{ width: "100%" }}
-                                placeholder="Tags"
-                                value={tag || null}
+                            <TagsInput
+                                // style={styles}
+                                // className="bg-gray-50 text-clip border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5"
+                                value={tags}
+                                onChange={setTags}
+                                name="tags"
+                                placeHolder="enter tags"
                             />
 
                         </div>
@@ -204,8 +217,12 @@ const Detail = () => {
                                             htmlFor="file-upload"
                                             className="relative cursor-pointer font-medium p-1 rounded-md text-red-600 hover:text--500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-gray-300"
                                         >
-                                            <span>Upload a file</span>
-                                            <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                                            <span>Upload product images</span>
+                                            <input multiple id="file-upload" name="file-upload"
+                                                type="file"
+                                                className="sr-only"
+                                                onChange={handleSelectImage}
+                                            />
                                         </label>
                                         <p className="pl-1">or drag and drop</p>
                                     </div>
@@ -222,7 +239,7 @@ const Detail = () => {
                                         <TrashIcon className='text-red-600 h-6 w-6' />
                                     </button>
                                 </div>
-                                {/* <img alt='product image' src={details.imageSrc} className='h-36 mx-auto' /> */}
+                                <img alt='product image' src={userInfo.filepreview} className='h-36 mx-auto' />
                             </div>
                             <div className='h-36 ring-1 ring-gray-300 rounded-lg hover:opacity-70 cursor-pointer relative'>
                                 <div className="absolute m-1 z-10 grid items-center justify-items-center top-0 right-0 h-8 w-8 text-white rounded-lg bg-red-600 bg-opacity-25 hover:bg-opacity-50">
@@ -232,7 +249,7 @@ const Detail = () => {
                                         <TrashIcon className='text-red-600 h-6 w-6' />
                                     </button>
                                 </div>
-                                {/* <img alt='product image' src={details.imageSrc} className='h-36 mx-auto ' /> */}
+                                {/* <img alt='product image' src={userInfo.filepreview2} className='h-36 mx-auto ' /> */}
                             </div>
                             <div className='h-36 ring-1 ring-gray-300 rounded-lg hover:opacity-70 cursor-pointer relative'>
                                 <div className="absolute m-1 z-10 grid items-center justify-items-center top-0 right-0 h-8 w-8 text-white rounded-lg bg-red-600 bg-opacity-25 hover:bg-opacity-50">
@@ -263,6 +280,7 @@ const Detail = () => {
 
                 {/* Dynamic Input  */}
                 <div className='grid grid-cols-1 md:grid-cols-2 items-start gap-2'>
+
                     {/* Details  */}
                     <div className='grid items-end gap-2'>
                         <h1>Product Details</h1>
@@ -273,8 +291,8 @@ const Detail = () => {
                                     <label htmlFor="detail" className="block mb-2 text-xs font-medium text-gray-900">Details</label>
                                     <input type="text" name="detail" id="detail" placeholder="Enter detail" required
                                         className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                        value={element.title || ""}
                                         onChange={(e) => handleChange(element.id, e)}
+                                        value={element.detail || ""}
 
                                     />
                                 </div>
@@ -325,11 +343,11 @@ const Detail = () => {
                     <div className='grid items-end gap-2'>
                         <h1>More Information</h1>
                         {moreInfos.map((element, index) => (
-                            <div className='grid grid-cols-10 items-end'>
+                            <div className='grid grid-cols-10 items-end' key={index}>
                                 <div className='grid grid-cols-2 gap-2 col-span-9'>
                                     <div>
-                                        <label htmlFor="info" className="block mb-2 text-xs font-medium text-gray-900">Title</label>
-                                        <input type="text" name="info" id="info" placeholder="Enter title" required
+                                        <label htmlFor="title" className="block mb-2 text-xs font-medium text-gray-900">Title</label>
+                                        <input type="text" name="title" id="title" placeholder="Enter title" required
                                             className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                             value={element.title || ""}
                                             onChange={(e) => handleMoreinfo(element.id, e)}
@@ -360,34 +378,6 @@ const Detail = () => {
                         </div>
                     </div>
 
-                    {/* Reviews  */}
-                    {/* <div className='grid items-end gap-2'>
-
-                        {reviews.map((element, index) => (
-                            <div className="flex gap-2 items-center" key={index}>
-                                <div className='flex'>
-                                    <div>
-                                        <label htmlFor="review" className="block mb-2 text-xs font-medium text-gray-900">Reviews</label>
-                                        <input type="text" name="review" id="review" value={element.review || ""}
-                                            onChange={(e) => handleReview(element.id, e)}
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5"
-                                            placeholder="Enter a review" required
-                                        />
-                                    </div>
-                                    {reviews.length != 1 && (
-                                        <button type="button" className="items-end flex" onClick={() => removeReview(element.id)}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2 mb-2 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </button>
-                                    )}
-                                </div>
-                            </div>
-                        ))}
-                        <div>
-                            <button className="w-auto text-white bg-black hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-lg text-xs sm:w-auto px-4 py-2 text-center" type="button" onClick={addReview}>Add</button>
-                        </div>
-                    </div> */}
                 </div>
                 <div className='p-2 bg-gray-200 rounded-lg mt-2 gap-2 flex items-end justify-end'>
                     <button className="w-auto text-white bg-red-600 hover:bg-black focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-lg text-xs sm:w-auto px-4 py-2 text-center" type='button'>Cancel</button>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/tag.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
