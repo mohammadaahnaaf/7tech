77d8a715fffa46465412fe96122912ffdbf262de
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2022-09-10 21:54:31 +0600

    nav style

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -138,9 +138,13 @@ function Navbar({ setSearchTerm }) {
                                         <Menu as="div" className="ml-3 relative">
                                             <div>
                                                 {useri && (
-                                                    <Menu.Button className="max-w-xs bg-white rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-white">
+                                                    <Menu.Button className="text-red-600 bg-red-600 bg-opacity-30 flex p-[8px] rounded-full relative hover:text-gray-200 focus:ring-2 focus:ring-red-800">
                                                         <span className="sr-only">Open user menu</span>
-                                                        <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                                                        {/* <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" /> */}
+                                                        <svg className="h-8 w-8 rounded-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                                        </svg>
+
                                                     </Menu.Button>
                                                 )}
                                             </div>
