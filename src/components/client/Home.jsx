cd03ba9c728b98cfd5273bb311b366a87da1bf17
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-02-21 14:21:06 +0600

    all

--- src/components/client/Home.jsx
+++ src/components/client/Home.jsx
@@ -1,11 +1,7 @@
-import Image from 'next/image'
 import React, { useState } from 'react'
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import axiosRoot from '../utils/axios-root';
-import Link from 'next/link';
-import { BasicNavbar, Footer } from '@seventech/shared';
-
 
 export function Banner() {
 
@@ -22,9 +18,9 @@ export function Banner() {
 
     return (
         <div className="max-w-full border-b-2 border-red-600 mx-auto">
-            {banners?.slice(0, 1).map((item, index) => (
+            {banners?.map((item, index) => (
                 <Carousel
-                    key={index}
+                    // key={index}
                     autoPlay
                     infiniteLoop
                     animationHandler="fade"
@@ -51,109 +47,5 @@ export function Banner() {
 
             )}
         </div>
-    )
-}
-
-export function Homes() {
-    return (
-        <div className="max-w-7xl min-h-screen mx-auto py-6 sm:px-6 lg:px-8">
-
-            <div className="grid grid-cols-2 gap-4 max-w-7xl mx-auto px-4 sm:px-6 lg:px-0">
-
-                <div className="col-start-1 col-end-3 sm:max-w-2xl lg:pt-40">
-                    <h1 className="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl">
-                        Opss!
-                    </h1>
-                    <p className="mt-4 text-2xl text-gray-500">
-                        The SevenTech website is under maintenance now. Stay connected with our social sites. We shall arrive very soon.
-                    </p>
-                    <div className='pt-10'>
-                        <Link href='/'>
-                            <a className="inline-block text-center bg-red-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-green-500">
-                                Home
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-
-                <div className="col-end-7 col-span-2 z-0">
-
-                    {/* Decorative image grid */}
-                    <div className="flex items-center space-x-6 lg:space-x-5">
-                        <div className="grid grid-cols-1 gap-y-6 lg:gap-y-8">
-                            <div className="w-40 h-64 rounded-lg overflow-hidden">
-                                <Image
-                                    height={640}
-                                    width={400}
-                                    // layout='fill'
-                                    // src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
-                                    src="/features/lady-airpod.jpg"
-                                    alt=""
-                                    className="w-full h-full object-center z-0 object-cover"
-                                />
-                            </div>
-                            <div className="w-40 h-64 rounded-lg overflow-hidden">
-                                <Image
-                                    height={640}
-                                    width={400}
-                                    src="/features/man-headphone.jpg"
-                                    // layout='responsive'
-                                    // src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg"
-                                    alt=""
-                                    className="w-full h-full z-0 object-center object-cover"
-                                />
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-1 gap-y-6 lg:gap-y-8">
-
-                            <div className="w-40 h-64 rounded-lg overflow-hidden">
-                                <Image
-                                    height={640}
-                                    width={400}
-                                    src="/features/lady-headphone.jpg"
-                                    // src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-04.jpg"
-                                    alt=""
-                                    className="w-full h-full z-0 object-center object-cover "
-                                />
-                            </div>
-
-                        </div>
-                        <div className="grid grid-cols-1 gap-y-6 lg:gap-y-8">
-                            <div className="w-40 h-64 rounded-lg overflow-hidden">
-                                <Image
-                                    height={640}
-                                    width={400}
-                                    src="/features/lady-headphone2.jpg"
-                                    // src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg"
-                                    alt=""
-                                    className="w-full h-full object-center object-cover"
-                                />
-                            </div>
-                            <div className="w-40 h-64 rounded-lg overflow-hidden">
-                                <Image
-                                    height={640}
-                                    width={400}
-                                    src="/features/lady2-headphone.jpg"
-                                    // src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg"
-                                    alt=""
-                                    className="w-full h-full z-0 object-center object-cover"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    )
-}
-
-export function Home() {
-    return (
-        <>
-            <BasicNavbar />
-            <Homes />
-            <Footer />
-        </>
     )
 }
