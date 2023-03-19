68ee4c459b563e2dd7ad6d5a370d0796a2e3b1c7
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-03-19 08:21:37 +0600

    category

--- src/components/client/categories/Category.jsx
+++ src/components/client/categories/Category.jsx
@@ -108,6 +108,7 @@ export function Category({ term }) {
     setSearchSubCats('')
     setName('')
     setCats(nam)
+    setMaxValue(50000)
     // console.log(nam)
   }
 
@@ -144,12 +145,12 @@ export function Category({ term }) {
               leaveFrom="translate-x-0"
               leaveTo="translate-x-full"
             >
-              <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
+              <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-sm flex-col overflow-y-auto bg-red-600 py-4 pb-12 shadow-xl">
                 <div className="flex items-center justify-between px-4">
-                  <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+                  <h2 className="text-lg font-semibold text-gray-200">Categories</h2>
                   <button
                     type="button"
-                    className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
+                    className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-red-600 p-2 text-gray-400"
                     onClick={() => setMobileFiltersOpen(false)}
                   >
                     <span className="sr-only">Close menu</span>
@@ -158,9 +159,9 @@ export function Category({ term }) {
                 </div>
 
                 {/* Filters */}
-                <form className="mt-4 border-t border-gray-200">
+                <form className="mt-4 border-t-2 border-gray-200">
                   <h3 className="sr-only">Categories</h3>
-                  <ul role="list" className="px-2 py-3 font-medium text-gray-900">
+                  <ul role="list" className="px-4 text-md grid gap-2 py-3 font-semibold text-gray-100">
                     {categories.map((category, index) => (
                       <li key={category.name}>
                         <Link href={`/category/${category.name}`}>
--- src/data/others.js
+++ src/data/others.js
@@ -32,7 +32,7 @@ export const about = [
     {
         _id: 1,
         title: 'Introduction',
-        description: "SevenTech is a Computer hardware, software & service supplier company established in August, 2016. We have grown consistently since inception and today we offer a wide range of computing services, products and Solutions. We trade as “Tech Land” under License Number: TRAD/DSCC/228998. BIN Number: 001614723-0201. TIN Number: 363773346098. We understand the role computers play today.  Computer systems are vital for both business and personal needs.  We are specialists in providing the most advanced and reliable computer systems, in installing effective software, and in providing ongoing, top-notch service.  Our systems, service, and consulting capabilities are key to providing high performance and reliability; thus insuring maximum productivity, profitability, and peace of mind.",
+        description: "SevenTech is a Computer hardware, software & service supplier company established in August, 2016. We have grown consistently since inception and today we offer a wide range of computing services, products and Solutions. We trade as SevenTech. We understand the role computers play today.  Computer systems are vital for both business and personal needs.  We are specialists in providing the most advanced and reliable computer systems, in installing effective software, and in providing ongoing, top-notch service.  Our systems, service, and consulting capabilities are key to providing high performance and reliability; thus insuring maximum productivity, profitability, and peace of mind.",
         topics: [
             'QUALITY',
             'SERVICE',
