80625956bcd094bc277323438586403a127f0067
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-03-20 16:39:39 +0600

    search

--- src/components/shared/Search.jsx
+++ src/components/shared/Search.jsx
@@ -31,19 +31,19 @@ export function Search({ setSearchTerm, searchButton }) {
                     <label htmlFor="search" className="mb-2 text-sm font-medium sr-only">Search</label>
                     <div className="relative w-full">
                         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <SearchIcon className='w-5 h-5 text-red-500 text-opacity-70' />
+                            <SearchIcon className='w-5 h-5 text-gray-200 text-opacity-70' />
                         </div>
                         <input
                             type="search"
                             id="search"
                             name="search"
                             onChange={handleSearch}
-                            className="block p-3 pl-10 w-full placeholder-red-500 placeholder-opacity-70 bg-transparent focus:ring-red-600 text-red-600 ring-red-600 border-red-600 focus:border-red-500 shadow-sm sm:text-sm"
-                            placeholder="Search..."
+                            className="block p-3 pl-10 w-full placeholder-gray-200 placeholder-opacity-70 bg-transparent focus:ring-red-600 text-red-600 ring-gray-200 border-gray-200 focus:border-red-600 shadow-sm sm:text-sm"
+                            placeholder="Search here..."
                             required
                         />
                         <button disabled={searchButton || false} type="submit" className={classNames(searchButton ? "cursor-not-allowed" : "",
-                            "px-4 flex absolute inset-y-2 right-2 items-center font-medium rounded-full text-sm py-1 bg-red-600 hover:bg-black text-black hover:text-red-600 hover:ring-1 ring-red-600")}
+                            "px-4 flex absolute inset-y-2 right-2 items-center font-medium rounded-full text-sm hover:bg-red-600 bg-black text-white hover:ring-red-600 ring-1 ring-gray-200")}
                         >Search
                         </button>
                     </div>
