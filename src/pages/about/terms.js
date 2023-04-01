6f2883c932e7e4c0713ecd36e77acd6122e03b79
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-04-02 02:18:37 +0600

    style

--- src/pages/about/terms.js
+++ src/pages/about/terms.js
@@ -25,7 +25,7 @@ function TermsPage() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Navbar />
-            <div className='min-h-screen bg-red-700 text-white border-t-0 border-red-600'>
+            <div className='min-h-screen text-black bg-white'>
                 <div className='max-w-7xl mx-auto grid'>
                     <h2 className='text-xl md:text-2xl font-semibold text-center py-4'>
                         Terms And Conditions
@@ -33,8 +33,8 @@ function TermsPage() {
                     <div className='grid items-start justify-start gap-5 md:gap-10 px-8 py-5'>
                         {terms.map((item, index) => (
                             <div key={index} className='grid gap-1'>
-                                <h3 className='text-sm font-medium md:text-2xl'>{index + 1}. {item.title}</h3>
-                                <p className='text-xs font-normal pl-3 md:pl-5 md:text-lg'>{item.description}</p>
+                                <h4 className='text-sm font-medium md:text-lg'>{index + 1}. {item.title}</h4>
+                                <p className='text-xs font-normal pl-3 md:pl-5 md:text-md'>{item.description}</p>
                             </div>
                         ))}
                     </div>
