b4ef43a6d898b4418944ad4fb0ca3bbd8a1912ff
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-08-19 20:02:35 +0600

    all

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Footers, Navbar } from '@seventech/shared'
-import { CategoryBar, NewCatBar } from '@seventech/client'
+import { NewCatBar } from '@seventech/client'
 
 export function Layout({ children, setSearchTerm }) {
 
     const [open, setOpen] = React.useState(false)
 
     return (
-        <>
+        <div suppressHydrationWarning={true}>
             <Navbar setOpen={setOpen} setSearchTerm={setSearchTerm} />
             {/* <CategoryBar open={open} setOpen={setOpen} /> */}
             <NewCatBar open={open} setOpen={setOpen} />
@@ -15,6 +15,6 @@ export function Layout({ children, setSearchTerm }) {
                 {children}
             </div>
             <Footers />
-        </>
+        </div>
     )
 }
